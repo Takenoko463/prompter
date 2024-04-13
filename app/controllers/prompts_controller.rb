867506ba3c41dafbc1b4989ef3527575affb3bff
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-13 12:54:55 +0900

    一覧ページ、投稿ページに渡すinstance変数を定義

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -1,5 +1,16 @@
 class PromptsController < ApplicationController
+  before_action :retribute_active_hash, only: [:index, :new]
   def index
     @prompts = Prompt.all
+  end
+
+  def new
+    @prompt = Prompt.new
+  end
+
+  private
+
+  def retribute_active_hash
+    @ais = Ai.all
   end
 end
