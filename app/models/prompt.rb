220fb28552a392172d11b768017e64d2cae4634e
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-13 13:29:22 +0900

    prompt一覧を表示

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,8 +1,10 @@
 class Prompt < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :ai
   with_options presence: true do
-    validates :title, comparison: { less_than: 31 }
-    validates :content, comparison: { greater_than: 5, less_than: 3000 }
-    validates :nick_name, comparison: { less_than: 31 }
+    validates :title, length: { maximum: 31 }
+    validates :content, length: { minimum: 5, maximum: 3000 }
+    validates :nick_name, length: { maximum: 31 }
     validates :ai_id, numericality: { other_than: 0 }
   end
   belongs_to :ai
