60d2e3b5b4cac0f5b0b4f5d123ec9c05105f7967
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-13 11:55:30 +0900

    Promptモデルに基本的なvalidationをかける

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,2 +1,8 @@
 class Prompt < ApplicationRecord
+  with_options presence: true do
+    validates :title, comparison: { less_than: 31 }
+    validates :content, comparison: { greater_than: 5, less_than: 3000 }
+    validates :nick_name, comparison: { less_than: 31 }
+    validates :ai_id, numericality: { other_than: 0 }
+  end
 end
