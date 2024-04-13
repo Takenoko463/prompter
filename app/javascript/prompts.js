6e4289209d0b4fd2eb96bb2346410ba8bbc9c64d
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-14 00:04:09 +0900

    非同期でのtuggle機能を実現 javascriptを動かす際にはtoggle機能を無効に

--- app/javascript/prompts.js
+++ app/javascript/prompts.js
@@ -1,23 +1,13 @@
- // ボタンクリック時に詳細情報を表示/非表示にする関数
- function toggleDetails() {
-     const details = this.nextElementSibling;
-     if (details.style.display === 'none') {
-         details.style.display = 'block';
-         this.textContent = 'プロンプト内容を隠す';
-     } else {
-         details.style.display = 'none';
-         this.textContent = 'プロンプト内容を表示';
-     }
- }
+// ボタンクリック時に詳細情報を表示/非表示にする関数
+/*$(window).on('load', function() {
+    $(hide)
+});*/
+$(window).on('turbo:load', function() {
+    $(hide)
+});
 
- function openDetails() {
-     // ボタンにイベントリスナーを追加
-     const buttons = document.querySelectorAll('.showDetails');
-     buttons.forEach(button => {
-         button.addEventListener('click', toggleDetails);
-     });
- }
-
-
- window.addEventListener('turbo:load', openDetails)
- window.addEventListener('turbo:render', openDetails)
+function hide() {
+    $('.prompt').click(function() {
+        $(this).find('.toggle').toggle();
+    })
+}
