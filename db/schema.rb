7960f70cc3d160fc3445a326d4604bd3c69f465a
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-15 01:54:07 +0900

    usersテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_12_152804) do
-  create_table "prompts", charset: "utf8mb4", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2024_04_14_160126) do
+  create_table "prompts", charset: "utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "content", null: false
     t.string "nick_name", null: false
     t.integer "ai_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
