cc547922687de6592065e019134268fcfdf280b6
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-15 20:00:40 +0900

    ヘルパーメソッドを定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def ip_to_md5_head8(ip)
+    Digest::MD5.hexdigest(ip)[0, 8]
+  end
+
+  # 使用しているuserのip
+  def current_ip
+    request.remote_ip
+  end
+
+  def current_ip_md5_head8
+    ip_to_md5_head8(current_ip)
+  end
 end
--- app/helpers/prompts_helper.rb
+++ app/helpers/prompts_helper.rb
@@ -1,2 +1,5 @@
 module PromptsHelper
+  def your_prompt?(prompt)
+    current_ip_md5_head8 == prompt.ip_md5_head8
+  end
 end
