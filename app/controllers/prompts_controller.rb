cf42bee654d285b8b34ea5ee044a8e9f0256cd87
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-15 20:17:35 +0900

    異なるipからのアクセスを排除

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -1,6 +1,7 @@
 class PromptsController < ApplicationController
   before_action :retribute_active_hash, only: [:index, :new, :edit]
   before_action :set_prompt, only: [:edit, :update, :destroy]
+  before_action :authenticate_ip!, only: [:edit, :update, :destroy]
   def index
     @prompts = Prompt.all
   end
@@ -38,12 +39,22 @@ class PromptsController < ApplicationController
     @prompt = Prompt.find(params[:id])
   end
 
-  def ip_to_md5(ip)
+  def ip_to_md5_head8(ip)
     Digest::MD5.hexdigest(ip)[0, 8]
   end
 
   def set_ip
     ip_plain = request.remote_ip
-    ip_to_md5(ip_plain)
+    ip_to_md5_head8(ip_plain)
+  end
+
+  def your_prompt?
+    set_ip == @prompt.ip_md5_head8
+  end
+
+  def authenticate_ip!
+    return if your_prompt?
+
+    redirect_to root_path
   end
 end
