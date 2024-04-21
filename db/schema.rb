cbc9bf2234eba0816b1020c58c185ad9707a6ab3
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-21 17:57:18 +0900

    カテゴリーをデータベースへ定義

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_21_063815) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_21_082619) do
+  create_table "categories", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "prompts", charset: "utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "content", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+themes = [
+  { name: "文学と創作" },
+  { name: "技術とプログラミング" },
+  { name: "ビジネスと経済" },
+  { name: "アートとデザイン" },
+  { name: "エンターテインメント" },
+  { name: "健康とフィットネス" },
+  { name: "科学と自然"},
+  { name: "インタビュー質問"},
+  {name: "その他"}
+]
+writing,programming,business,art,entertainment,fitness,science,interview=Category.create(themes)
+
+writing_children=[{name: "小説"},{name: "詩"},{name: "キャラクター開発"}]
+writing.children.create(writing_children)
+
+programming_children=[{name: "Web開発"},{name: "モバイル開発"},{name: "データサイエンス"},{name: "DX"},{name: "プログラミン学習"}]
+programming.children.create(programming_children)
