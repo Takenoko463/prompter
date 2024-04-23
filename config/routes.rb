6ffd4024d5ac1feaf06b43f7b47efad98721ac20
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-23 19:40:21 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "prompts#index"
   resources :prompts,except:[:index] do
-    resources :likes ,only:[:create,:destroy]
+    resource :likes ,only:[:create,:destroy]
     resources :comments ,only:[:index,:new,:create,:edit,:update,:destroy]
   end
   resources :categories, only:[:show] do
