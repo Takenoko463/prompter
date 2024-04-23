44635875d76fc145ce331ff257e8686204d5b9d6
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-23 19:51:18 +0900

    likes投稿の基本設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,21 @@
 class LikesController < ApplicationController
+  before_action :set_prompt
   def create
+    like = Like.new(like_params)
+    like.valid?
+    like.save
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def set_prompt
+    @prompt = Prompt.find(params[:prompt_id])
+  end
+
+  def like_params
+    { prompt_id: params[:prompt_id], ip_md5_head8: current_ip_md5_head8 }
   end
 end
