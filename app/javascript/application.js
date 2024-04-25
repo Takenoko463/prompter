b2286edc6a18189ea2d084fd77f119a0a7df7251
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-25 16:48:55 +0900

    bootstrapよりpopperを先に読む

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "jquery"
 import "jquery_ujs"
-import "bootstrap"
 import "popper"
+import "bootstrap"
 import "@hotwired/turbo-rails"
 import "controllers"
 import "prompts"
