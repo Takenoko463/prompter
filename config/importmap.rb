932633094a6cd81d452fcc7fd40ab396b38e8394
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-25 09:17:57 +0900

    エラーが出るため、preload撤回

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,9 +6,9 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "prompts", to:"prompts.js"
-pin "jquery", to: "jquery.min.js", preload: true
-pin "jquery_ujs", to: "jquery_ujs.js", preload: true
+pin "jquery", to: "jquery.min.js"
+pin "jquery_ujs", to: "jquery_ujs.js"
 pin "categories", to:"categories.js"
 pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.5.2/js/all.js"
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js",preload: true
-pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js", preload: true
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
