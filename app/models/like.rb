1b2d8f29a484b40bcc91a579a060af202b324a0d
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-27 20:10:52 +0900

    同じpromptに同一ipからいいねを押せない

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,5 @@
 class Like < ApplicationRecord
+  validates :prompt, uniqueness: { scope: :ip }
   belongs_to :ip
   belongs_to :prompt
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -21,6 +21,12 @@ RSpec.describe Like, type: :model do
         @like.valid?
         expect(@like.errors.full_messages).to include 'Prompt must exist'
       end
+      it '同じpromptに2回投稿できない' do
+        @like.save
+        another_like = FactoryBot.build(:like, ip: @like.ip, prompt: @like.prompt)
+        another_like.valid?
+        expect(another_like.errors.full_messages).to include 'Prompt has already been taken'
+      end
     end
   end
 end
