f455f5f4491e385eddc8bd97ea5e035ee586850a
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-27 21:40:32 +0900

    ip コントローラーに関するテスト

--- spec/requests/ips_request_spec.rb
+++ spec/requests/ips_request_spec.rb
@@ -4,8 +4,11 @@ RSpec.describe 'Ips', type: :request do
   describe 'Create /ips' do
     it 'createアクションにリクエストすると正常にレスポンスが返ってくる' do
       get root_path
-      authenticate_or_request_with_http_basic(ENV['BASIC_AUTH_USER'], ENV['BASIC_AUTH_PASSWORD'])
       expect(response.status).to eq 200
+    end
+    it 'createアクションにリクエストするとsessionにip_idが保存されている' do
+      get root_path
+      expect(session[:ip_id]).to be_a_kind_of(Integer)
     end
   end
 end
