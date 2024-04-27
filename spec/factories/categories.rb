54ba66864bc5ed58ad7abfe676a1467e42159a8f
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-27 21:16:24 +0900

    カテゴリーのfactory bot定義

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,9 +1,13 @@
 FactoryBot.define do
-  factory :category_root do
+  factory :category_parent do
     name { 'all' }
+  end
+  factory :category do
+    name { 'current_category' }
+    ancestry { '0' }
   end
   factory :category_child do
     name { 'child' }
-    ancestry { 0 }
+    ancestry { '0/1' }
   end
 end
