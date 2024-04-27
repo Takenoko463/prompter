f1c8e906e54c6801e9bb4f6bc9eae728628d8361
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-28 01:19:43 +0900

    before actionは定義

--- spec/requests/prompts_request_spec.rb
+++ spec/requests/prompts_request_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Prompts', type: :request do
+  before do
+    # ipをデータベースに登録
+    @ip = FactoryBot.create(:ip)
+    # categoryをrootから孫まで定義
+    @category_root = FactoryBot.create(:category, id: 0, name: '全て', ancestry: nil)
+    FactoryBot.create(:category, id: 1, name: 'カテゴリ1', ancestry: '0')
+    FactoryBot.create(:category, id: 2, name: 'カテゴリ2', ancestry: '0/1')
+    @prompt = FactoryBot.create(:prompt, category: @category_root, ip: @ip)
+  end
   describe 'GET /prompts' do
-    before do
-      # ipをデータベースに登録
-      @ip = FactoryBot.create(:ip)
-      # categoryをrootから孫まで定義
-      @category_root = FactoryBot.create(:category, id: 0, name: '全て', ancestry: nil)
-      FactoryBot.create(:category, id: 1, name: 'カテゴリ1', ancestry: '0')
-      FactoryBot.create(:category, id: 2, name: 'カテゴリ2', ancestry: '0/1')
-      @prompt = FactoryBot.create(:prompt, category: @category_root, ip: @ip)
-    end
     it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
       get prompts_path
       expect(response.status).to eq 200
@@ -18,6 +18,12 @@ RSpec.describe 'Prompts', type: :request do
     it 'indexアクションにリクエストするとレスポンスに投稿済みのpromptのテキストが存在する' do
       get prompts_path
       expect(response.body).to include(@prompt.content)
+    end
+  end
+  describe 'NEW /prompts' do
+    it 'newアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get new_prompt_path
+      expect(response.status).to eq 200
     end
   end
 end
