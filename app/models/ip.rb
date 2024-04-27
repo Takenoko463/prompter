274ddf4b2477dac989d1e33b64214d1dc7ccbfdb
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-27 19:20:18 +0900

    ip_md5_head8は存在し、かつ8文字

--- app/models/ip.rb
+++ app/models/ip.rb
@@ -1,5 +1,5 @@
 class Ip < ApplicationRecord
-  validates :ip_md5_head8, length: { maximum: 8 }
+  validates :ip_md5_head8, presence: true, length: { is: 8 }
 
   has_many :prompts
   has_many :likes
