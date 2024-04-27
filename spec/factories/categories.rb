344d9a259acce2a26d018bade59e08c916d85c38
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-27 21:12:34 +0900

    categoryをfactoryで定義

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
-  factory :category do
-    
+  factory :category_root do
+    name { 'all' }
+  end
+  factory :category_child do
+    name { 'child' }
+    ancestry { 0 }
   end
 end
