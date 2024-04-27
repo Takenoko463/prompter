4bf28cc747928bf10463072a71d81c84141d7e8f
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-27 19:27:40 +0900

    ip登録して、トップページへ案内

--- app/controllers/ips_controller.rb
+++ app/controllers/ips_controller.rb
@@ -1,8 +1,10 @@
 class IpsController < ApplicationController
   include IpsHelper
   def create
+    return if ip_registered?
+
     ip = Ip.find_by(ip_md5_head8: current_ip_md5_head8)
-    Ip.create(ip_md5_head8: current_ip_md5_head8) unless ip
-    session[:current_ip_md5_head8] = current_ip_md5_head8
+    ip ||= Ip.create(ip_md5_head8: current_ip_md5_head8)
+    session[:ip_id] = ip.id
   end
 end
