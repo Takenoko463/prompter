b414b3e967f49343bc435021fd42b54bd26b8bf9
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-27 13:58:43 +0900

    ipテーブルを参照するために、過去のipカラム作成migrationを削除

--- db/migrate/20240415070357_add_ip_to_prompts.rb
+++
@@ -1,5 +0,0 @@
-class AddIpToPrompts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :prompts, :ip, :string, null: false
-  end
-end
--- db/migrate/20240415100759_rename_ip_column_to_prompts.rb
+++
@@ -1,5 +0,0 @@
-class RenameIpColumnToPrompts < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :prompts, :ip, :ip_md5_head8
-  end
-end
