d6f2bf52194a6a2e8ce9a45f404d9c4594871219
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-27 22:58:06 +0900

    categoryモデルの作成

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,13 +1,6 @@
 FactoryBot.define do
-  factory :category_parent do
-    name { 'all' }
-  end
   factory :category do
     name { 'current_category' }
-    ancestry { '0' }
-  end
-  factory :category_child do
-    name { 'child' }
-    ancestry { '0/1' }
+    ancestry { nil }
   end
 end
--- spec/requests/prompts_request_spec.rb
+++ spec/requests/prompts_request_spec.rb
@@ -2,8 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Prompts', type: :request do
   describe 'GET /prompts' do
+    before do
+      FactoryBot.create(:category, id: 0, name: '全て', ancestry: nil)
+      FactoryBot.create(:category, id: 1, name: 'カテゴリ1', ancestry: '0')
+      FactoryBot.create(:category, id: 2, name: 'カテゴリ2', ancestry: '0/1')
+    end
     it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
       get prompts_path
+      expect(response.status).to eq 200
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みのpromptのテキストが存在する' do
     end
