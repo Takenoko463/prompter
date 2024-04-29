e7450546d29f7b2a6920fd1ef883dc0ad9efef13
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-29 17:53:11 +0900

    prompt回答をテストbotに設定

--- spec/factories/prompts.rb
+++ spec/factories/prompts.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :prompt do
     title { Faker::Alphanumeric.alphanumeric(number: 10) }
-    nick_name { Faker::Alphanumeric.alphanumeric(number: 10) }
     content { Faker::Alphanumeric.alphanumeric(number: 10) }
+    nick_name { Faker::Alphanumeric.alphanumeric(number: 10) }
     ai_id { Faker::Number.between(from: 1, to: 2) }
+    answer { Faker::Alphanumeric.alphanumeric(number: 100) }
     association :ip
     association :category
   end
