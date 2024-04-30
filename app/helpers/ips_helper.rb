92990cf43477f97d7c248e6395f845996e5d04b5
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-30 09:30:57 +0900

    ip名をモデルからかrequestからかで区別

--- app/helpers/ips_helper.rb
+++ app/helpers/ips_helper.rb
@@ -3,18 +3,17 @@ module IpsHelper
     Digest::MD5.hexdigest(ip)[0, 8]
   end
 
-  def set_ip
-    ip_plain = request.remote_ip
-    ip_to_md5_head8(ip_plain)
-  end
-
   # 通信内容から取得したip
-  def current_ip
+  def current_ip_plain
     request.remote_ip
   end
 
+  def set_ip
+    ip_to_md5_head8(current_ip_plain)
+  end
+
   def current_ip_md5_head8
-    ip_to_md5_head8(current_ip)
+    ip_to_md5_head8(current_ip_plain)
   end
 
   def your_ip?(ip_md5_head8)
@@ -28,10 +27,14 @@ module IpsHelper
 
   # sessionをいじってからのデータ操作を防ぐ
   def legitimate_ip?
-    your_ip?(Ip.find(session[:ip_id]).ip_md5_head8)
+    your_ip?(current_ip.ip_md5_head8)
   end
 
   def authenticate_ip!
     redirect_to root_path unless ip_registered?
+  end
+
+  def current_ip
+    Ip.find(session[:ip_id])
   end
 end
