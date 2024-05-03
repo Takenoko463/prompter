95d4f03dae46658458273fdc0544584b557523bb
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-05-04 01:36:56 +0900

    コメント内容に最小文字の制限を設けない

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,6 @@ class Comment < ApplicationRecord
 
   with_options presence: true do
     validates :nick_name, length: { maximum: 30 }
-    validates :content, length: { minimum: 5, maximum: 3000 }
+    validates :content, length: { maximum: 3000 }
   end
 end
