177c88cf1b56fca1a84eb38e25f6c3f114c17e17
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-05-04 13:20:21 +0900

    モデルテスト

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :comment do
-    
+    nick_name { Faker::Alphanumeric.alphanumeric(number: 10) }
+    content { Faker::Alphanumeric.alphanumeric(number: 10) }
+    association :ip
+    association :prompt
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+  describe 'コメント投稿' do
+    context 'コメント投稿をできるとき' do
+      it 'nick_name,content,ip,promptを持っている' do
+        expect(@comment).to be_valid
+      end
+    end
+    context 'コメント投稿をできないとき' do
+      it 'nick_nameなしでは投稿できない' do
+        @comment.nick_name = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include "Nick name can't be blank"
+      end
+      it 'contentなしでは投稿できない' do
+        @comment.content = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include "Content can't be blank"
+      end
+      it 'ipを特定できていないと投稿できない' do
+        @comment.ip = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include 'Ip must exist'
+      end
+      it 'promptを特定できていないと投稿できない' do
+        @comment.prompt = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include 'Prompt must exist'
+      end
+    end
+  end
 end
