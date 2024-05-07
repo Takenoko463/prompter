8df230c3ce0f86cc2913f855cb5e333b45501dab
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-05-07 19:42:35 +0900

    検索フォームをボタンにカーソルを合わせるだけで取得 遷移をより素早く

--- app/javascript/prompt_form.js
+++ app/javascript/prompt_form.js
@@ -27,27 +27,22 @@ const postForm = () => {
         })
 };
 const searchForm = () => {
-    $('#collapseSearch')
-        .on({
-            'show.bs.collapse': function() {
-                console.log($("#promptSearchButton").data('url'));
-                $.ajax({
-                        url: $("#promptSearchButton").data('url'),
-                        type: 'GET',
-                        dataType: 'html'
-                    })
-                    // Ajax通信が成功したら発動
-                    .done((data) => {
-                        $(this).find('.collapse-body').html(data);
-                    })
-                    // Ajax通信が失敗したら発動
-                    .fail((jqXHR, textStatus, errorThrown) => {
-                        alert('Ajax通信に失敗しました。');
-                        console.log("jqXHR          : " + jqXHR.status); // HTTPステータスを表示
-                        console.log("textStatus     : " + textStatus); // タイムアウト、パースエラーなどのエラー情報を表示
-                        console.log("errorThrown    : " + errorThrown); // 例外情報を表示
-                    });
-
-            }
-        })
+    $(document).on('mouseenter', 'button#promptSearchButton', function() {
+        $.ajax({
+                url: $(this).data('url'),
+                type: 'GET',
+                dataType: 'html'
+            })
+            // Ajax通信が成功したら発動
+            .done((data) => {
+                $("#collapseSearch").find('.collapse-body').html(data);
+            })
+            // Ajax通信が失敗したら発動
+            .fail((jqXHR, textStatus, errorThrown) => {
+                alert('Ajax通信に失敗しました。');
+                console.log("jqXHR          : " + jqXHR.status); // HTTPステータスを表示
+                console.log("textStatus     : " + textStatus); // タイムアウト、パースエラーなどのエラー情報を表示
+                console.log("errorThrown    : " + errorThrown); // 例外情報を表示
+            });
+    })
 };
