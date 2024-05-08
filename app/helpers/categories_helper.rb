cf2292271dcbf5195c6afc62e6c554009ee2680a
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-05-08 12:30:41 +0900

    カテゴリ名変更が検索からも行えるように調整

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,6 +1,7 @@
 module CategoriesHelper
   def set_current_category_at_session
-    session[:current_category_id] = params[:category_id].present? ? params[:category_id] : 0
+    params_category_id = params[:category_id] || params[:q].try(:[], :subtree_category) || 0
+    session[:current_category_id] = params_category_id
   end
 
   def current_category_id
